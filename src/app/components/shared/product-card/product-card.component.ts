04c46ec6889fbca83f03c9925133eb92f249ebec
atsa21 <ts21ann@gmail.com>
2023-05-24 14:48:41 +0300

    add amount prod validation

--- src/app/components/shared/product-card/product-card.component.ts
+++ src/app/components/shared/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { ProductItem } from 'src/app/models/product.interface';
 
 @Component({
@@ -13,33 +14,77 @@ export class ProductCardComponent implements OnInit {
 
   prodAddedIndex!: number;
   isAdded = false;
+  prodAmount!: FormControl;
+  
   private order: any;
 
   ngOnInit(): void {
+    this.prodAmount = new FormControl(0, [Validators.min(1), Validators.max(20)]);
     this.getOrder();
   }
 
-  getOrder(): void {
+  get amount() {
+    return this.prodAmount.value;
+  }
+
+  private getOrder(): void {
     this.order = JSON.parse(localStorage.getItem('order') || '[]');
     this.prodAddedIndex = this.order.findIndex((el: any) => el.name === this.product.name);
     this.isAdded = !!(this.prodAddedIndex + 1);
+    if(this.isAdded) {
+      this.prodAmount.setValue(this.order[this.prodAddedIndex].amount);
+    }
   }
 
   addItemToCart(): void {
     this.getOrder();
-    if(this.prodAddedIndex !== -1) {
-      const newAmount = this.order[this.prodAddedIndex].amount + 1;
-      this.order[this.prodAddedIndex].amount = newAmount;
-    } else {
-      const newItem = {
-        name: this.product.name, 
-        price: this.product.price, 
-        amount: 1
-      };
-      this.order ? this.order.push(newItem) : this.order = [newItem];
-    }
+    const newItem = {
+      name: this.product.name, 
+      price: this.product.price, 
+      amount: 1
+    };
+    this.order ? this.order.push(newItem) : this.order = [newItem];
     this.isAdded = true;
     localStorage.setItem('shop', JSON.stringify(this.shop));
     localStorage.setItem('order', JSON.stringify(this.order));
+    this.prodAmount.setValue(1);
+  }
+
+  setAmount(event: KeyboardEvent): void {
+    if(event.key === '-' || event.key === 'e') {
+      this.prodAmount.setValue(1);
+    }
+    this.setItemAmount();
+  }
+
+  incrementAmount(): void {
+    this.getOrder();
+    this.prodAmount.setValue(this.amount + 1);
+    this.setItemAmount();
+  }
+
+  decrementAmount(): void {
+    this.getOrder();
+    this.prodAmount.setValue(this.amount - 1);
+    !this.amount ? this.order = this.order.filter((el: any) => el.name !== this.product.name) : this.setItemAmount();
+    this.updateOrder();
+  }
+
+  setItemAmount(): void {
+    if(this.prodAmount.valid) {
+      this.order[this.prodAddedIndex].amount = this.amount;
+      this.updateOrder();
+    }
+  }
+
+  setValidAmount(): void {
+    if(this.prodAmount.invalid) {
+      this.updateOrder();
+    }
+  }
+
+  private updateOrder(): void {
+    localStorage.setItem('order', JSON.stringify(this.order));
+    this.getOrder();
   }
 }
