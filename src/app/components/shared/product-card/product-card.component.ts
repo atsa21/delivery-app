e864d9fc3041a67b7320b592e38244fc965dfc8b
atsa21 <ts21ann@gmail.com>
2023-05-24 12:30:16 +0300

    added product set order in local store

--- src/app/components/shared/product-card/product-card.component.ts
+++ src/app/components/shared/product-card/product-card.component.ts
@@ -1,13 +1,39 @@
-import { Component, Input } from '@angular/core';
-import { Product, ProductItem } from 'src/app/models/product.interface';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductItem } from 'src/app/models/product.interface';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
 
   @Input() product!: ProductItem;
+  prodAddedIndex!: number;
+  private order: any;
 
+  ngOnInit(): void {
+    this.getOrder();
+  }
+
+  getOrder(): void {
+    this.order = JSON.parse(localStorage.getItem('order') || '[]');
+    this.prodAddedIndex = this.order.findIndex((el: any) => el.name === this.product.name);
+  }
+
+  addItemToCart(): void {
+    this.getOrder();
+    if(this.prodAddedIndex !== -1) {
+      const newAmount = this.order[this.prodAddedIndex].amount + 1;
+      this.order[this.prodAddedIndex].amount = newAmount;
+    } else {
+      const newItem = {
+        name: this.product.name, 
+        price: this.product.price, 
+        amount: 1
+      };
+      this.order ? this.order.push(newItem) : this.order = [newItem];
+    }
+    localStorage.setItem('order', JSON.stringify(this.order));
+  }
 }
