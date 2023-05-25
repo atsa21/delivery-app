886db7321cc257114c7b9b905efdd834790bf2e7
atsa21 <ts21ann@gmail.com>
2023-05-25 22:21:33 +0300

    deploy app

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -18,6 +19,7 @@ mongoose.connect("mongodb+srv://ts21ann:" + process.env.MONGO_ATLAS_PW + "@skill
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -34,5 +36,8 @@ app.use((req, res, next) => {
 
 app.use("/api/shops", shopsRoutes);
 app.use("/api/orders", ordersRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "http://localhost:3000/api"
+    apiUrl: "http://deliver-server-env.eba-xfvrp9uq.us-east-2.elasticbeanstalk.com/api"
 };
