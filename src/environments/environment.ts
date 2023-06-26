6b7d7869f50d164ea11c14cf91f60745e2960470
atsa21 <ts21ann@gmail.com>
2023-06-26 12:13:00 +0300

    changed env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "http://deliver-server-env.eba-xfvrp9uq.us-east-2.elasticbeanstalk.com/api"
+    apiUrl: "https://delivery-angular-app.glitch.me/api"
 };
