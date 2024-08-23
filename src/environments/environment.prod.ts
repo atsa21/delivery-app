d6a5bffe85a83493a76dd74527d53ef57e32c410
Anna Sakhaudinova <104850911+atsa21@users.noreply.github.com>
2024-08-23 14:02:21 +0300

    fixed localhost run

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: "https://delivery-angular-app.glitch.me/api"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: "https://delivery-angular-app.glitch.me/api"
+    production: false,
+    apiUrl: "http://localhost:3000/api"
 };
